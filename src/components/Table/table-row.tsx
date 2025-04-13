5f3d6bf4a05c180efc0e2c9c01a7c9dc5a5ec01b
Gabr1elSouza <gablgdario03@gmail.com>
2025-04-13 15:10:33 -0300

    Finish

--- src/components/Table/table-row.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-interface TableHeaderProps extends ComponentProps<"tr"> {}
-
-export function TableRow(props: TableHeaderProps) {
-  return <tr className="border-b border-white hover:bg-white/5" {...props} />;
-}
